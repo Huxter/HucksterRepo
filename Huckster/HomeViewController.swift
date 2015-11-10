900276995a9f846ea93af38eddb97c2c3fa834e7
Jaytu72 <jaytu72@gmail.com>
2015-11-10 16:12:16 -0500

    Second commit

---
+++ Huckster/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Huckster
+//
+//  Created by Jason Tu on 11/10/15.
+//  Copyright © 2015 Jason Tu, Daniel Chen. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Huckster/ProfileViewController.swift
+++ Huckster/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  ProfileViewController.swift
 //  Huckster
 //
 //  Created by Jason Tu on 11/10/15.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class ProfileViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
